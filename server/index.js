ef9eca37cf42bf9f45d88fffd631a41137c70656
Chotu Kumar <ck4001252@gmail.com>
2025-01-05 20:16:13 +0530

    cors allows from origin

--- server/index.js
+++ server/index.js
@@ -12,18 +12,13 @@ const app = express();
 mongoConnect();
 
 
-const corsOptions = {
-  origin: 'https://joinsphere.vercel.app', 
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
-
 app.use(express.json());
-app.use(cors(corsOptions)); 
+app.use(cors()); 
 app.use(morgan('dev'));
 
-// Routes
+
 app.use('/api/users', userRoutes);
+
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
